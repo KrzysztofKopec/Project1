91fa59e4507f0560f302b8ed5b2b713a09f44ac1
Krzysztof Kopec <kontociepok@gmail.com>
2021-11-20 18:58:03 +0100

    Create new project

---
+++ src/main/java/com/kontociepok/springgradlehibernateh2/SpringGradleHibernateH2Application.java
@@ -0,0 +1,13 @@
+package com.kontociepok.springgradlehibernateh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleHibernateH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleHibernateH2Application.class, args);
+	}
+
+}
