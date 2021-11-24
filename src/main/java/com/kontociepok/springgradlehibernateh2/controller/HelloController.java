de2183f5b0ae0958815e08090cf2fc6c9f92454b
Krzysztof Kopec <kontociepok@gmail.com>
2021-11-24 14:51:41 +0100

    update HelloController

--- src/main/java/com/kontociepok/springgradlehibernateh2/controller/HelloController.java
+++ src/main/java/com/kontociepok/springgradlehibernateh2/controller/HelloController.java
@@ -1,14 +1,47 @@
 package com.kontociepok.springgradlehibernateh2.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.kontociepok.springgradlehibernateh2.model.User;
+import com.kontociepok.springgradlehibernateh2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
 
+    private UserRepository userRepository;
+
+
+    @Autowired
+    public HelloController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
-        return "Hello "+name;
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("/users")
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user) {
+        userRepository.save(user);
+        return user;
+    }
+
+    @GetMapping("/users/{userId}")
+    public User getEmployee(@PathVariable long userId) {
+        User user = userRepository.findById(userId);
+        return user;
+    }
+    @DeleteMapping("/users/{userId}")
+    public String deleteUser(@PathVariable long userId){
+        userRepository.deleteById(userId);
+        return "Delete User no Id: "+ userId;
     }
 }
