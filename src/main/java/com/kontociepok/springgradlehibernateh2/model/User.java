66e8d34b2469652473e3c9a80cc775cc880b4912
Krzysztof Kopec <kontociepok@gmail.com>
2021-11-24 14:48:58 +0100

    Create model User

---
+++ src/main/java/com/kontociepok/springgradlehibernateh2/model/User.java
@@ -0,0 +1,100 @@
+package com.kontociepok.springgradlehibernateh2.model;
+
+
+public class User {
+
+    public enum TypeUser {STUDENT, TEACHER;}
+
+    private Long id;
+
+    private String login;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String pesel;
+
+    private User.TypeUser typeUser;
+
+    public User(Long id, String login, String password, String firstName, String lastName, String pesel, TypeUser typeUser) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pesel = pesel;
+        this.typeUser = typeUser;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public TypeUser getTypeUser() {
+        return typeUser;
+    }
+
+    public void setTypeUser(TypeUser typeUser) {
+        this.typeUser = typeUser;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", pesel=" + pesel +
+                ", typeUser=" + typeUser +
+                '}';
+    }
+}
