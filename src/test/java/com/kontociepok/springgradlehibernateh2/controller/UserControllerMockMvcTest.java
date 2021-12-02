7f2a212d701d7a1f28c42a9989d1bb8821737f23
Krzysztof Kopec <kontociepok@gmail.com>
2021-12-02 20:07:58 +0100

    Create test controller MockMvc

---
+++ src/test/java/com/kontociepok/springgradlehibernateh2/controller/UserControllerMockMvcTest.java
@@ -0,0 +1,90 @@
+package com.kontociepok.springgradlehibernateh2.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kontociepok.springgradlehibernateh2.model.User;
+import com.kontociepok.springgradlehibernateh2.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(HelloController.class)
+public class UserControllerMockMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    HelloController helloController;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    void shouldReturnListSizeAndNameUsers() throws Exception{
+        //given
+        User user = new User(1L,"Krzysztof");
+        User user1 = new User(2L,"Bartek");
+        List<User> users = List.of(user,user1);
+        given(userRepository.findAll()).willReturn(users);
+
+        //when
+        ResultActions result = mockMvc.perform(get("/users").contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        result.andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].firstName").value("Krzysztof"))
+                .andExpect(jsonPath("$[1].firstName").value("Bartek"));
+    }
+    @Test
+    void shouldReturnUserById() throws Exception{
+        //given
+        User user = new User(1L,"Krzysztof");
+        //userRepository.save(user);
+        given(userRepository.findById(1L)).willReturn(user);
+        //when(userRepository.findById(anyLong())).thenReturn(user);
+
+        //when
+        ResultActions result = mockMvc.perform(get("/users/1").contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        result.andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.firstName").value("Krzysztof"));
+    }
+    @Test
+    void shouldReturnSaveUser() throws Exception{
+        User user = new User(1L,"Tomasz");
+        //given(userRepository.save(any(User.class)).willReturn(user);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        ResultActions result = mockMvc.perform(post("/addUser")
+                        .content(new ObjectMapper().writeValueAsString(new User(1L,"Tomasz")))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk()).andExpect(jsonPath("$.id").exists());
+                //.andExpect(jsonPath("$.id").value("1L"))
+                //.andExpect(jsonPath("$.firstName").value("Tomasz"));
+    }
+
+}
