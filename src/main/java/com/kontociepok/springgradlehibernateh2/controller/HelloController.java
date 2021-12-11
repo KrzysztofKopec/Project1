73cb698b1a2535581a4b072046c98ade516b3b65
Krzysztof Kopec <kontociepok@gmail.com>
2021-12-11 16:42:09 +0100

    correct code testMockMvc and HelloController

--- src/main/java/com/kontociepok/springgradlehibernateh2/controller/HelloController.java
+++ src/main/java/com/kontociepok/springgradlehibernateh2/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.kontociepok.springgradlehibernateh2.controller;
 
 import com.kontociepok.springgradlehibernateh2.model.User;
+import com.kontociepok.springgradlehibernateh2.model.UserDTO;
 import com.kontociepok.springgradlehibernateh2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class HelloController {
@@ -25,23 +27,34 @@ public class HelloController {
     }
 
     @GetMapping("/users")
-    public List<User> findAll() {
-        return userRepository.findAll();
+    public List<UserDTO> findAll(){
+        return userRepository.findAll().stream().map(this::convertToUserDTO).collect(Collectors.toList());
     }
+
 
     @PostMapping("/addUser")
-    public User addUser(@RequestBody User user) {
-        return userRepository.save(user);
-        //return user;
+    public UserDTO addUser(@RequestBody UserDTO userDTO) {
+        User user = new User(userDTO.getId(), userDTO.getFirstName());
+        userRepository.save(user);
+        return userDTO;
     }
 
+
     @GetMapping("/users/{userId}")
-    public User getUser(@PathVariable long userId) {
-        User user = userRepository.findById(userId);
-        return user;
+    public UserDTO getUser(@PathVariable long userId){
+        return convertToUserDTO(userRepository.findById(userId));
     }
+
     @DeleteMapping("/users/{userId}")
     public String deleteUser(@PathVariable long userId){
-        return userRepository.deleteById(userId);
+        userRepository.deleteById(userId);
+        return "Delete User Id: "+ userId;
+    }
+
+    private UserDTO convertToUserDTO(User user){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstName(user.getFirstName());
+        return userDTO;
     }
 }
--- src/test/java/com/kontociepok/springgradlehibernateh2/controller/UserControllerMockMvcTest.java
+++ src/test/java/com/kontociepok/springgradlehibernateh2/controller/UserControllerMockMvcTest.java
@@ -4,21 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kontociepok.springgradlehibernateh2.model.User;
 import com.kontociepok.springgradlehibernateh2.repository.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -59,9 +54,8 @@ public class UserControllerMockMvcTest {
     void shouldReturnUserById() throws Exception{
         //given
         User user = new User(1L,"Krzysztof");
-        //userRepository.save(user);
+
         given(userRepository.findById(1L)).willReturn(user);
-        //when(userRepository.findById(anyLong())).thenReturn(user);
 
         //when
         ResultActions result = mockMvc.perform(get("/users/1").contentType(MediaType.APPLICATION_JSON));
@@ -74,7 +68,7 @@ public class UserControllerMockMvcTest {
     @Test
     void shouldReturnSaveUser() throws Exception{
         User user = new User(1L,"Tomasz");
-        //given(userRepository.save(any(Us).willReturn(user);
+
         when(userRepository.save(new User(1L,"Tomasz"))).thenReturn(user);
 
         ResultActions result = mockMvc.perform(post("/addUser")
